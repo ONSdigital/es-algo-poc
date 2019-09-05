bf6eef1e3a0cea2e06b498853805cbd194c7fcaf
Ashutosh Jindal <Ash.Jindal@ext.ons.gov.uk>
2019-09-05 13:54:13 +0100

    Added restriction on AWS provider version
    Task: LU-3268

--- versions.tf
+++ versions.tf
@@ -1,8 +1,6 @@
-# ----------------------------------------------------------------------------------------------------------------------
-# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
-# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
-# ----------------------------------------------------------------------------------------------------------------------
-
 terraform {
-  required_version = ">= 0.12.8"
+  required_providers {
+    aws = "2.26.0"
+  }
+  required_version = "0.12.8"
 }
